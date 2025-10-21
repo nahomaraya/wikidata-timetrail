6d8c9e55c3eccbe507727baf556440c97592a91b
nahomaraya <nahomaraya8@gmail.com>
2025-10-21 00:50:44 -0700

    removed read only redis

--- src/config/upstash.config.ts
+++ src/config/upstash.config.ts
@@ -3,8 +3,6 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('upstash', () => ({
     redisUrl: process.env.UPSTASH_REDIS_REST_URL,
     redisToken: process.env.UPSTASH_REDIS_REST_TOKEN,
-    redisReadOnly: process.env.KV_REST_API_READ_ONLY_TOKEN,
-    redisDefaultUrl: process.env.REDIS_URL,
     redisDb: "upstash-kv-red-yacht"
     
 }));
